c713ba063b6d4390374773b7720d91d4271b74e0
shihd1 <46411183+shihd1@users.noreply.github.com>
2024-12-09 02:38:56 -0500

    Initial Commit

---
+++ Warlords 3Tests/Warlords_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Warlords_3Tests.swift
+//  Warlords 3Tests
+//
+//  Created by Darren Shih on 2024/12/9.
+//
+
+import Testing
+@testable import Warlords_3
+
+struct Warlords_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Warlords 3UITests/Warlords_3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Warlords_3UITestsLaunchTests.swift
+//  Warlords 3UITests
+//
+//  Created by Darren Shih on 2024/12/9.
+//
+
+import XCTest
+
+final class Warlords_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
